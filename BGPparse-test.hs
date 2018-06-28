fe51dda6bf7b484cf82da558da223c94c7c2111b
Nicholas Hart <nic@hdb3.net>
2018-06-28 13:11:12 +0100

    working encode/decode from selftest PoV

--- BGPparse-test.hs
+++ BGPparse-test.hs
@@ -3,19 +3,26 @@ import BGPparse
 import Hexdump
 import Data.Binary
 import Data.Int
-import Data.ByteString.Lazy
+import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString as B
+import Data.ByteString.Char8(pack)
 
-main = do
-          let openmsg = BGPOpen 1000 600 65550
-          print openmsg
-          let encMsg = encode openmsg :: ByteString
-          print $ simpleHex $ toStrict encMsg
-          -- let decMsg = decode encMsg :: BGPMessage
-          -- print decMsg
+fromString = pack
 
-          let decMsg = decodeOrFail encMsg :: Either (ByteString, Int64, String) (ByteString, Int64, BGPMessage)
-          case decMsg of (Left (_,_,s)) -> do print $ "failed to decode message:" ++ s
-                         (Right ( _,_,decMsg')) -> do print decMsg'
+main = do identityCheck "BGPOpen" $ BGPOpen 1000 600 65550
+          identityCheck "BGPKeepalive" BGPKeepalive
+          identityCheck "BGPNotify" $ BGPNotify 99 99 (fromString "Error data")
+          identityCheck "BGPUpdate" $ BGPUpdate (fromString "Withdrawn routes") (fromString "Path Attributes") (fromString "nlri")
 
--- toStrict lbs = B.concat $ toChunks lbs
+identityCheck :: String -> BGPMessage -> IO ()
+identityCheck name bgpMsg = do putStrLn $ "identityCheck on " ++ name
+                               let encMsg = encode bgpMsg :: L.ByteString
+                               let decMsg = decodeOrFail encMsg :: Either (L.ByteString, Int64, String) (L.ByteString, Int64, BGPMessage)
+                               case decMsg of (Left (_,_,s)) -> do print $ "failed to decode message:" ++ s
+                                                                   print $ simpleHex $ L.toStrict encMsg
+                                              (Right ( _,_,decMsg')) -> if decMsg' == bgpMsg then print "success"
+                                                                        else do print "recoded message is not identical"
+                                                                                print bgpMsg
+                                                                                print $ simpleHex $ L.toStrict encMsg
+                                                                                print decMsg'
+                               putStrLn ""
--- BGPparse.hs
+++ BGPparse.hs
@@ -1,69 +1,78 @@
 
 module BGPparse where
-import Data.ByteString (ByteString,replicate)
+--import Data.ByteString (ByteString,replicate)
+import qualified Data.ByteString as B
 import Data.Word
 import Data.Binary
 import Data.Binary.Get
 import Data.Binary.Put
 import Control.Monad(unless,fail)
+import qualified Data.ByteString.Lazy as L
 
-_BGPOpen = 1
-_BGPUpdate = 2
-_BGPNotify = 3
-_BGPKeepalive = 4
-_BGPMarker = Data.ByteString.replicate 16 0xff
+_BGPOpen = 1 :: Word8
+_BGPUpdate = 2 :: Word8
+_BGPNotify = 3 :: Word8
+_BGPKeepalive = 4 :: Word8
+_BGPMarker = B.replicate 16 0xff
 _BGPVersion = 4 :: Word8
-
--- data BGPMessage = BGPOpen BGPOpen | BGPKeepalive | BGPNotify BGPNotify | BGPUpdate BGPUpdate deriving Show
-
-{-
-data BGPOpen = BGPOpen { myAutonomousSystem :: Word16
-                         , holdTime :: Word16
-                         , bgpID :: Word32
-                         -- TODO - Optional Parameters 
-                         }
-
-data BGPNotify = BGPNotify { errorCode :: Word8
-                             , errorSubcode :: Word8
-                             , errorData :: ByteString }
-
-data BGPUpdate = BGPUpdate { withdrawnRoutes :: ByteString
-                             , pathAttributes :: ByteString
-                             , nlri :: ByteString } 
-
--}
 
 data BGPMessage = BGPOpen { myAutonomousSystem :: Word16, holdTime :: Word16, bgpID :: Word32 }
                   | BGPKeepalive
-                  | BGPNotify { errorCode :: Word8, errorSubcode :: Word8, errorData :: ByteString }
-                  | BGPUpdate { withdrawnRoutes :: ByteString, pathAttributes :: ByteString, nlri :: ByteString }
-                    deriving Show
+                  | BGPNotify { errorCode :: Word8, errorSubcode :: Word8, errorData :: B.ByteString }
+                  | BGPUpdate { withdrawnRoutes :: B.ByteString, pathAttributes :: B.ByteString, nlri :: B.ByteString }
+                    deriving (Show,Eq)
 
 instance Binary BGPMessage where
+
     put (BGPOpen myAutonomousSystem holdTime bgpID) = do putByteString _BGPMarker
-                                                         put (16+2+1+1+2+2+4 :: Word16)
-                                                         put (_BGPOpen :: Word8)
-                                                         put (_BGPVersion :: Word8)
-                                                         put myAutonomousSystem
-                                                         put holdTime
-                                                         put bgpID
+                                                         putWord16be $ 16+2+1+1+2+2+4
+                                                         putWord8 _BGPOpen
+                                                         putWord8 _BGPVersion
+                                                         putWord16be myAutonomousSystem
+                                                         putWord16be holdTime
+                                                         putWord32be bgpID
+
+    put (BGPUpdate withdrawnRoutes pathAttributes nlri) = do putByteString _BGPMarker
+                                                             putWord16be $ 16+2+1
+                                                             putWord8 _BGPUpdate
+                                                             putWord16be $ fromIntegral $ B.length withdrawnRoutes
+                                                             putByteString withdrawnRoutes
+                                                             putWord16be $ fromIntegral $ B.length pathAttributes
+                                                             putByteString pathAttributes
+                                                             putByteString nlri
+
+    put (BGPNotify errorCode errorSubcode errorData) = do putByteString _BGPMarker
+                                                          putWord16be $ 16+2+1
+                                                          putWord8 _BGPNotify
+                                                          putWord8 errorCode
+                                                          putWord8 errorSubcode
+                                                          putByteString errorData
+
+    put BGPKeepalive                                = do putByteString _BGPMarker
+                                                         putWord16be $ 16+2+1
+                                                         putWord8 _BGPKeepalive
 
     get = do marker <- getByteString 16
              unless (marker == _BGPMarker) (fail "Bad marker")
              msgLen <- getWord16be
              msgType <- getWord8
-             case msgType of _BGPOpen -> do msgVer  <- getWord8
-                                            unless (msgVer == _BGPVersion) (fail "Bad version")
-                                            myAutonomousSystem <- getWord16be
-                                            holdTime <- getWord16be
-                                            bgpID <- getWord32be
-                                            return $ BGPOpen myAutonomousSystem holdTime bgpID
-
--- 
--- bgpHeader typeCode | typeCode >= _BGPOpen && typeCode <= _BGPKeepalive =
-    
--- unparse :: BGPMessage -> ByteString
--- unparse BGPKeepalive = bgpHeader _BGPKeepalive
-
--- parse :: ByteString -> Either String BGPMessage
--- parse _ = BGPKeepalive
+             case msgType of
+                             1 -> do msgVer  <- getWord8
+                                     unless (msgVer == _BGPVersion) (fail "Bad version(Open)")
+                                     myAutonomousSystem <- getWord16be
+                                     holdTime <- getWord16be
+                                     bgpID <- getWord32be
+                                     return $ BGPOpen myAutonomousSystem holdTime bgpID
+                             2   -> do withdrawnRoutesLength <- getWord16be
+                                       withdrawnRoutes <- getByteString $ fromIntegral withdrawnRoutesLength
+                                       pathAttributesLength <- getWord16be
+                                       pathAttributes <- getByteString $ fromIntegral pathAttributesLength
+                                       nlri <- getRemainingLazyByteString
+                                       return $ BGPUpdate withdrawnRoutes pathAttributes $ L.toStrict nlri
+                             3   -> do errorCode <- getWord8
+                                       errorSubcode <- getWord8
+                                       errorData <- getRemainingLazyByteString
+                                       return $ BGPNotify errorCode errorSubcode $ L.toStrict errorData
+                             4 -> do unless (msgLen == 16+2+1) (fail "Bad length (Keepalive)")
+                                     return BGPKeepalive
+                             _ -> do fail "Bad type code"
