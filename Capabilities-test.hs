e42c0c933c5969f4e7a48a114e5f915f7e993f6e
Nicholas Hart <nic@hdb3.net>
2018-07-05 11:47:02 +0100

    refactored tests

--- Capabilities-test.hs
+++ Capabilities-test.hs
@@ -11,28 +11,37 @@ module Main where
 import Capabilities
 import Hexdump
 
+capmp = CapMultiprotocol 0 0
+capAS4 = CapAS4 0x00010f0f
+capGrR = CapGracefulRestart False 0
+testList = [
+             ("empty list",[]),
+             ("singleton list",[capGrR]),
+             ("long list",[capmp, capAS4, capGrR])
+           ] :: [(String,[Capability])]
+
 main = do
     putStrLn "capability test"
     testEncodings
     putStrLn ""
-    testOptionalParameters
+    runTests "testOptionalParameters" testOptionalParameters testList
 
 testEncodings = do
     putStrLn "testEncodings"
     putStrLn "done"
 
-testOptionalParameters = do
-    putStrLn "testOptionalParameters"
-    let capmp = CapMultiprotocol 0 0
-        capAS4 = CapAS4 0x00010f0f
-        capGrR = CapGracefulRestart False 0
-        runtest d ps = do
-            let params = buildOptionalParameters ps
-            putStrLn $ d ++ ":" ++ show ps
-            putStrLn $ "encoded: " ++ ( simpleHex params)
-            putStrLn $ "decoded: " ++ ( show $ parseOptionalParameters params )
-            putStrLn ""
-    runtest "empty list" []
-    runtest "singleton list" [capGrR]
-    runtest "long list" [capmp, capAS4, capGrR]
+runTests desc f tests = do
+    putStrLn desc
+    mapM  f tests
     putStrLn "done"
+
+testOptionalParameters (d,ps) = do
+        let params = buildOptionalParameters ps
+            recodedParams = parseOptionalParameters params
+        putStrLn $ d ++ ":" ++ show ps
+        putStrLn $ "encoded: " ++ ( simpleHex params)
+        putStrLn $ "decoded: " ++ show recodedParams
+        if (recodedParams == ps) then
+            putStrLn "conversion OK" else
+            putStrLn "*** conversion FAIL!!!"
+        putStrLn ""
