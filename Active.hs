1cf615c8867ddb90f95e538127d55c606df59f06
Nicholas Hart <nic@hdb3.net>
2018-07-10 15:32:55 +0100

    add configurable parameters to ACtive.hs

--- Active.hs
+++ Active.hs
@@ -1,5 +1,6 @@
 module Main where
 -- active TCP speaker
+import System.Environment
 import Control.Exception(bracket)
 import Network.Socket
 import Common
@@ -8,13 +9,45 @@ import BGPparse
 import Capabilities
 
 main :: IO ()
-main = bracket open close (bgpFSM local remote)
+main = do
+          (local,remote) <- getConfig
+          bracket open close (bgpFSM local remote)
   where
-    local = BGPOpen 65520 40 (read "192.168.0.1") [ CapAS4 65520,  CapGracefulRestart False 0]
-    remote = BGPOpen 65521 40 (read "192.168.0.2") [ CapAS4 65521,  CapGracefulRestart False 0]
+    getConfig = do
+        args <- getArgs
+        let local = if length args > 0
+            then
+                parseParams $ args !! 0
+            else
+                BGPOpen 65520 40 (read "192.168.0.1") [ CapAS4 65520,  CapGracefulRestart False 0]
+        let remote = if length args > 1
+            then
+                parseParams $ args !! 1
+            else
+                BGPOpen 0 0 (read "0.0.0.0") []
+        print local
+        print remote
+        return (local,remote)
+
     open = do
         putStrLn "begin:: "
         sock <- socket AF_INET Stream defaultProtocol
         connect sock (SockAddrInet bgpPort ipV4_localhost)
         putStrLn "connected:: "
         return sock
+
+    -- here is an example of a valid parameter list:
+    -- "30,40,192.168.1.1,CapAS4,65520,CapGracefulRestart,False,0"
+    --
+    parseParams ps = BGPOpen (read $ ws !! 0) (read $ ws !! 1) (read $ ws !! 2) (parseCapabilities (drop 3 ws)) where
+        ws = myWords ps
+
+    parseCapabilities [] = []
+    parseCapabilities ("CapAS4":as:cx) = CapAS4 (read as) : parseCapabilities cx
+    parseCapabilities ("CapGracefulRestart":b:w:cx) = CapGracefulRestart (read b) (read w) : parseCapabilities cx
+
+    myWords :: String -> [String]
+    myWords "" = []
+    myWords (',':ws) = myWords ws
+    myWords ws = w : myWords ws' where
+        (w,ws') = break (',' ==) ws
