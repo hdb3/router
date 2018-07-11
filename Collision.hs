c99413cb67f1e43ac2b927102620c7fba02111ba
Nicholas Hart <nic@hdb3.net>
2018-07-11 16:17:08 +0100

    first pass running Collision.hs and Collision-test.hs

---
+++ Collision.hs
@@ -0,0 +1,40 @@
+module Collision where
+import Network.Socket
+import Data.IP
+import Control.Concurrent
+-- data CollisionDetector = CollisionDetector ( MVar ( [ Session ] ) )
+type CollisionDetector = MVar ( [ Session ] )
+data Session = Session { sessionBgpid :: IPv4, sessionAddr :: SockAddr, sessionTid :: ThreadId, established :: Bool }  deriving (Eq,Show)
+
+purge :: ThreadId -> [ Session ] -> [ Session ]
+purge tid sx = reverse $ purge' tid [] sx  where
+    -- purge' :: [ Session ] 
+    purge' tid ret [] = ret
+    purge' tid ret (s:sx) | tid == (sessionTid s) = purge' tid ret sx
+                          | otherwise = purge' tid (s:ret) sx
+
+raceCheck :: CollisionDetector -> IPv4 -> SockAddr -> IO (Maybe Session)
+raceCheck c bgpid addr = do
+    sessions <- takeMVar c
+    let collision = lookupBGPid bgpid sessions
+    threadID <- myThreadId
+    putMVar c $ ( Session bgpid addr threadID False ) : sessions
+    return collision
+    where
+    lookupBGPid id [] = Nothing
+    lookupBGPid id (s:sx) | id == (sessionBgpid s) = Just s
+                          | otherwise = lookupBGPid id sx 
+
+registerEstablished :: CollisionDetector -> IPv4 -> SockAddr -> IO ()
+registerEstablished c bgpid addr = do
+    sessions <- takeMVar c
+    threadID <- myThreadId
+    let sessions' = purge threadID sessions
+    putMVar c $ ( Session bgpid addr threadID True ) : sessions'
+
+deregister :: CollisionDetector -> IO ()
+deregister c = do
+    threadID <- myThreadId
+    sessions <- takeMVar c
+    putMVar c $ purge threadID sessions
+    
