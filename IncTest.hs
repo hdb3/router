634a6a63e8c7146796aa7c144f8e591f2198c71c
Nicholas Hart <nic@hdb3.net>
2018-07-18 16:54:24 +0100

    defining a list include operation

---
+++ IncTest.hs
@@ -0,0 +1,27 @@
+module Main where
+import Data.List(delete)
+
+inc [] _ = True
+inc ax [] | not (null ax) = False
+inc ax (b:bx) = inc (delete b ax) bx
+
+
+
+main = mapM incTest [
+    ([1],[2,3],False),
+    ([2],[2,3],True),
+    ([],[2,3],True),
+    ([],[],True),
+    ([2],[],False),
+    ([1,2],[1,2,3],True),
+    ([1,2],[2,3],False),
+    ([1,2],[2,3,4,5,6,7,1],True),
+    ([1,2],[2,3,4,5,6,7,8],False)]
+
+incTest (a,b,p) = do
+    let p' = inc a b
+    if p == p'  then
+        putStr "OK "
+    else
+        putStr "*** Fail "
+    print (a,b,p)
