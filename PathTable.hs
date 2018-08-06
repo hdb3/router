f6cb1b9f7ac492af5d97dc32c00bf33918c689ac
Nicholas Hart <nic@hdb3.net>
2018-08-06 09:59:21 +0100

    WiP

--- PathTable.hs
+++ PathTable.hs
@@ -13,6 +13,8 @@ module PathTable where
 
 import Data.IntMap.Strict
 import PathAttributes
+import FarmHash(hash64)
+
 data Cost = Cost { preference :: Word32, pathLength :: Word8, med :: Word32, fromEBGP :: Bool, peerBGPID :: Word32 }
 instance Ord Cost where
   compare (Cost pr1 pl1 med1 fEBGP1 peer1)(Cost pr2 pl2 med2 fEBGP2 peer2)
@@ -23,4 +25,24 @@ instance Ord Cost where
       | otherwise = compare peer1 peer2
  
 data PathTableEntry = PathTableEntry { ptePath :: [PathAttribute], pteCost :: Cost, pteNewPath [PathAttribute] }
-type PathTable = IntMap PathTableEntry
+newtype PathTable = PathTable { map :: IntMap PathTableEntry } -- ignoring cost function
+type UpdatePathFunction = ([PathAttribute] -> [PathAttribute])
+makeSimpleUpdatePathFunction :: Word32 -> ASPath2  -> UpdatePathFunction -- TODO - support AS2 and AS4.....
+makeSimpleUpdatePathFunction nextHop addAS = (updateNextHop nextHop) (addAS newAS) 
+-- newtype PathTable = PathTable { map :: IntMap PathTableEntry, costFunction :: CostFunction
+-- type CostFunction = [PathAttribute] -> Cost
+-- makeCostFunction :: ([PathAttribute] -> Word32) -> Word32 -> CostFunction
+-- makeCostFunction prefFunction peerBGPID = f where
+--     f path = 
+
+{- PathTable insert: provide the prefix count so ref count can be maintained.
+ -  This also updates the ref count for the dereferenced paths.
+ -  Unreferenced paths are removed.  A path table reference is returned.
+-}
+
+pathTableInsert :: PathTable -> ([PathAttribute],B.ByteString) -> Int -> (Int,PathTable)
+-- in this simple version we are not going to bother about removing old path entries and managing reference counts....
+--
+pathTableInsert pt (route,bytes) pfxCount = (hash,pt') where
+    hash = hash64 bytes
+    pt' = 
