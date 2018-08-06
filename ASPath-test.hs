73cdcf80fe39462c4d4bf39b7f637602804ac7d6
Nicholas Hart <nic@hdb3.net>
2018-08-06 18:41:13 +0100

    atto seprate in ASPath4.hs, tested with ASPath-test.hs

--- ASPath-test.hs
+++ ASPath-test.hs
@@ -5,39 +5,39 @@ import qualified Data.ByteString.Lazy as L
 
 import Common
 import ASPath
+import qualified ASPath4 as AS4
 
 asSequence2 = ASSequence . as2list
 asSet2 = ASSet . as2list
 asSequence4 = ASSequence . as4list
 asSet4 = ASSet . as4list
+
+path0 = ASPath [] :: ASPath16
+path1 = ASPath [seg0] :: ASPath16
+path2 = ASPath [seg1] :: ASPath16
+path3 = ASPath [seg1,seg2] :: ASPath16
+seg0  = asSequence2 []
+seg1  = asSet2 [1,2,3]
+seg2  = asSequence2 [6,5,4]
+pathAttribute3 = ASPath2 path3 :: ASPath42
+path40 = ASPath [] :: ASPath32
+path41 = ASPath [seg4a] :: ASPath32
+path42 = ASPath [seg4b] :: ASPath32
+path43 = ASPath [seg4c] :: ASPath32
+path44 = ASPath [seg4b,seg4c] :: ASPath32
+seg4a  = asSequence4 []
+seg4b  = asSet4 [1,2,3]
+seg4c  = asSequence4 [6,5,4]
+pathAttribute44 = ASPath4 path44 :: ASPath42
+
 main = do
-    let path0 = ASPath [] :: ASPath16
-        path1 = ASPath [seg0] :: ASPath16
-        path2 = ASPath [seg1] :: ASPath16
-        path3 = ASPath [seg1,seg2] :: ASPath16
-        seg0  = asSequence2 []
-        seg1  = asSet2 [1,2,3]
-        seg2  = asSequence2 [6,5,4]
-        pathAttribute3 = ASPath2 path3 :: ASPath42
-    -- encDecSeq seg0
-    -- encDecSeq seg1
-    -- test path0
-    -- test path1
-    -- test ( ASPath [seg0] :: ASPath2)
-    -- test ( ASPath [ seg0, seg0] :: ASPath2)
     mapM_ test2 [path0,path1,path2,path3]
     test42 False pathAttribute3
-    let path40 = ASPath [] :: ASPath32
-        path41 = ASPath [seg4a] :: ASPath32
-        path42 = ASPath [seg4b] :: ASPath32
-        path43 = ASPath [seg4c] :: ASPath32
-        path44 = ASPath [seg4b,seg4c] :: ASPath32
-        seg4a  = asSequence4 []
-        seg4b  = asSet4 [1,2,3]
-        seg4c  = asSequence4 [6,5,4]
-        pathAttribute44 = ASPath4 path44 :: ASPath42
     test42 True pathAttribute44
     mapM_ test4 [path40,path41,path42,path43,path44]
+    prependTest
+
+prependTest = do
     let path42' = asPrePend 42 path42
         path2'  = asPrePend 42 path2
         path43' = asPrePend 42 path43
@@ -47,28 +47,18 @@ main = do
     print path43'
     print path3'
 
--- encDecSeq :: ASNumber asn => ASSegment asn -> IO ()
-encDecSeq seq = do
-    let enc = encode seq
-        dec = decode enc
-        ok = seq == dec
-    putStrLn ""
-    print ok
-    putStrLn $ "original: " ++ show seq
-    putStrLn $ "encoded:  " ++ simpleHex' enc
-    putStrLn $ "decoded:  " ++ show dec
-    putStrLn " ------------------"
-
 test42 :: Bool -> ASPath42 -> IO()
 test42 isAS4 path = do
                putStrLn "test42"
                let enc = encode path
                    -- dec = decode enc :: ASPath2
                    dec = if isAS4 then decodeAsASPath4 enc else decodeAsASPath2 enc
+                   dec' = AS4.decode enc
                print path
                putStrLn ""
                putStrLn $ "encoded: " ++ simpleHex' enc
                putStrLn $ "decoded: " ++ show dec
+               putStrLn $ "atto decoded: " ++ show dec'
                putStrLn " ------------------"
 
 test2 :: ASPath16 -> IO()
@@ -76,10 +66,12 @@ test2 path = do
                putStrLn ""
                let enc = encode path
                    dec = decode enc :: ASPath16
+                   dec' = AS4.decode enc
                print path
                putStrLn ""
                putStrLn $ "encoded: " ++ simpleHex' enc
                putStrLn $ "decoded: " ++ show dec
+               putStrLn $ "atto decoded: " ++ show dec'
                putStrLn " ------------------"
 
 test4 :: ASPath32 -> IO()
@@ -87,8 +79,10 @@ test4 path = do
                putStrLn ""
                let enc = encode path
                    dec = decode enc :: ASPath32
+                   dec' = AS4.decode enc
                print path
                putStrLn ""
                putStrLn $ "encoded: " ++ simpleHex' enc
                putStrLn $ "decoded: " ++ show dec
+               putStrLn $ "atto decoded: " ++ show dec'
                putStrLn " ------------------"
---
+++ ASPath4.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DataKinds #-}
+module ASPath4 where
+import Data.Binary
+-- import Data.Binary.Get
+-- import Data.Binary.Put
+import Data.Word
+import Data.List(foldl')
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import Control.Monad
+import Control.Applicative
+import Data.Attoparsec.ByteString
+import Data.Attoparsec.Binary
+import Data.Either.Combinators
+
+import RFC4271
+import Codes
+import Common
+import ASPath
+
+decode = fromRight' . parseOnly (path <* endOfInput) . L.toStrict 
+type ASSegment2 = ASSegment Word16
+type ASSegment4 = ASSegment Word32
+
+path :: Parser ASPath42
+path = path4 <|> path2
+path2 :: Parser ASPath42
+path2 = do
+    segs <- many' asSetOrSeq2
+    endOfInput
+    return $ ASPath2 $ ASPath segs
+
+asSetOrSeq2 :: Parser ASSegment2
+asSetOrSeq2 = do
+    segType <- satisfy isSetOrSeq
+    asCount <- anyWord8
+    rvals <- count (fromIntegral asCount) anyWord16be
+    return $ if segType == enumASSet then ASSet rvals else ASSequence rvals where
+        isSetOrSeq b = b == enumASSet || b == enumASSequence
+
+path4 :: Parser ASPath42
+path4 = do
+    segs <- many' asSetOrSeq4
+    endOfInput
+    return $ ASPath4 $ ASPath segs
+
+asSetOrSeq4 :: Parser ASSegment4
+asSetOrSeq4 = do
+    segType <- satisfy isSetOrSeq
+    asCount <- anyWord8
+    rvals <- count (fromIntegral asCount) anyWord32be
+    return $ if segType == enumASSet then ASSet rvals else ASSequence rvals where
+        isSetOrSeq b = b == enumASSet || b == enumASSequence
+
+{-
+segments2 :: Parser ASPath16
+segments2 = do
+    -- segs <- many' segment2
+    segs <- many' asSetOrSeq2
+    return $ ASPath segs
+segment2 :: Parser ASSegment2
+segment2 = asSet2 <|> asSeq2
+
+asSeq2 :: Parser ASSegment2
+asSeq2 = do
+    word8 enumASSequence
+    asCount <- anyWord8
+    rvals <- count (fromIntegral asCount) anyWord16be
+    return $ ASSequence rvals
+
+asSet2 :: Parser ASSegment2
+asSet2 = do
+    word8 enumASSet
+    asCount <- anyWord8
+    rvals <- count (fromIntegral asCount) anyWord16be
+    return $ ASSet rvals
+-}
