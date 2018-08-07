f31c04e76befb76c9ce959485391ea59beeff863
Nicholas Hart <nic@hdb3.net>
2018-08-07 21:36:25 +0100

    add BGPData.hs

---
+++ BGPData.hs
@@ -0,0 +1,17 @@
+module 4xiBGPPeerData where
+
+{- peer data holds persistent/static data about a BGP session peer
+ - this can be useful for BGP operatins like path selection
+-}
+
+import Data.Word
+data GlobalData = GlobalData { myAS :: Word32 }
+
+data PeerData = PeerData { globalData :: GlobalData,
+                           isExternal :: Bool,
+                           bgpID :: Word32,
+                           interfaceAddress :: Word32,
+                           localPref :: Word32,
+
+data RouteData =  RouteData { peerData :: PeerData,
+                              cost :: RouteCost,
