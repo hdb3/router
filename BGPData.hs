0a441801f699e5207e245ee4a2ff060ed9a8e918
Nicholas Hart <nic@hdb3.net>
2018-08-16 15:40:04 +0100

    defluff BGPData.hs

--- BGPData.hs
+++ BGPData.hs
@@ -11,7 +11,6 @@ data GlobalData = GlobalData { myAS :: Word32
                              , myBGPid :: IPv4
                              }
                             deriving Eq
-                             -- deriving (Show,Eq)
 
 data PeerData = PeerData { globalData :: GlobalData
                          ,  isExternal :: Bool
@@ -22,7 +21,6 @@ data PeerData = PeerData { globalData :: GlobalData
                          ,  localPref :: Word32
                          }
                             deriving Eq
-                         -- deriving (Show,Eq)
 
 data RouteData =  RouteData { peerData :: PeerData
                             , pathLength :: Word8
@@ -31,7 +29,6 @@ data RouteData =  RouteData { peerData :: PeerData
                             , fromEBGP :: Bool
                             }
                             deriving Eq
-                            -- deriving (Show,Eq)
 
 instance Show GlobalData where
     show gd = " router: " ++ show ( myBGPid gd )
@@ -46,11 +43,3 @@ instance Ord RouteData where
 
   compare rd1 rd2 = compare (localPref (peerData rd1), pathLength rd1, origin rd1, med rd1, not $ fromEBGP rd1, peerBGPid (peerData rd1), peerIPv4 (peerData rd1))
                             (localPref (peerData rd2), pathLength rd2, origin rd2, med rd2, not $ fromEBGP rd2, peerBGPid (peerData rd2), peerIPv4 (peerData rd2))
-{-
-data Cost = Cost { preference :: Word32, pathLength :: Word8, med :: Word32, fromEBGP :: Bool, peerBGPID :: Word32 } deriving Eq
-      | pr1 /= pr2 = compare pr1 pr2
-      | pl1 /= pl2 = compare pl1 pl2
-      | med1 /= med2 = compare med1 med2
-      | fEBGP1 /= fEBGP2 = compare fEBGP1 fEBGP2
-      | otherwise = compare peer1 peer2
--}
