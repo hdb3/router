6e5b46c24e24d343927057caa61818a157dafe14
Nicholas Hart <nic@hdb3.net>
2018-08-22 17:00:48 +0100

    WiP

--- Overlap.hs
+++ Overlap.hs
@@ -1,18 +1,25 @@
 {-#LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
 module Overlap where
-
+import Data.Bits
 import Prefixes
 
 data Tree a = Tree a [Tree a]
 
-type PrefixTree = Tree IPrefix
+type PrefixTree = Tree Prefix
 
-defaultRoute :: IPrefix
+defaultRoute :: Prefix
 defaultRoute = "0.0.0.0/0"
 
 root = Tree defaultRoute []
 
 data Overlaps = Includes | IncludedBy | NoOverlap
 
-comp :: IPrefix -> IPrefix -> Overlaps
-comp a b = NoOverlap
+comp :: Prefix -> Prefix -> Overlaps
+comp (Prefix (!l1,!v1)) (Prefix (!l2,!v2)) | l1 == l2 = NoOverlap
+                                           |  l1 < l2 && (mask l1 v1) == ( mask l1 v2) = Includes
+                                           |  l1 > l2 && (mask l2 v1) == ( mask l2 v2) = IncludedBy 
+                                           | otherwise = NoOverlap
+                                           where mask l v = v .&. (shiftR (fromIntegral l) 0xffffffff) 
+
+insert x (Tree a 
