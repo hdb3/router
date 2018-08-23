68a97660c3912e134943fd2e8c61c9cdc10042e2
Nicholas Hart <nic@hdb3.net>
2018-08-23 15:13:05 +0100

    drop bogons in OverlapReader.hs

--- OverlapReader.hs
+++ OverlapReader.hs
@@ -1,14 +1,26 @@
 module Main where
 import System.IO
+import Data.List(sortOn)
 
+import Prefixes
 import BGPReader(readRib)
 import Overlap
 
 main = do
     rib <- readRib
     putStrLn $ "got " ++ show (length rib) ++ " routes"
-    print (last rib)
+    -- print (last rib)
     let allPrefixes = concatMap snd rib
-    putStrLn $ "got " ++ show (length allPrefixes) ++ " prefixes"
-    let t = Overlap.fromList allPrefixes
+        -- drop default if present and also every thing longer than /24
+        (real,fake) = span ((25 >) . l) $ dropWhile ((0 ==) . l) $ sortOn l allPrefixes
+        l (Prefix (a,b)) = a
+        
+    putStrLn $ "got " ++ show (length real) ++ " prefixes"
+    print $ ("real", take 5 real)
+    print $ ("fake",fake)
+{-
+    let t = Overlap.fromList sortedAllPrefixes
     putStrLn $ "tree contains " ++ show (size t)
+    putStrLn $ "tree height   " ++ show (height t)
+    putStrLn $ "tree width    " ++ show (width t)
+-}
