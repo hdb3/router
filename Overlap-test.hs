8559044cc03e7453bcd6a851e50e2cad5b092313
Nicholas Hart <nic@hdb3.net>
2018-08-23 09:14:08 +0100

    overlap seems to work with simple data

--- Overlap-test.hs
+++ Overlap-test.hs
@@ -3,5 +3,13 @@ module Main where
 import Prefixes
 import Overlap
 
+fl :: [Prefix] -> PrefixTree
+fl = fromList . ("0.0.0.0/0" :)
+
 main = do
+    print $ fl []
     print $ singleton "192.168.0.0/24"
+    print $ fl ["192.168.0.0/24"]
+    print $ fl ["192.168.0.0/24","192.168.0.0/24"]
+    print $ fl ["192.168.1.0/24","192.168.0.0/24"]
+    print $ fl ["192.168.1.0/24","192.168.1.128/25"]
--- Overlap.hs
+++ Overlap.hs
@@ -1,37 +1,35 @@
 {-#LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BangPatterns #-}
 module Overlap where
+import Data.List(foldl')
 import Data.Bits
 import Prefixes
 
-data Tree a = Tree a [Tree a]
+type PrefixTree = Node Prefix
 
-type PrefixTree = Tree Prefix
+data Overlaps = Includes | IncludedBy | NoOverlap | Equal deriving Eq
+class Overlap a where
+    comp :: a -> a -> Overlaps
 
-defaultRoute :: Prefix
-defaultRoute = "0.0.0.0/0"
-
--- root = Tree defaultRoute []
-
-data Overlaps = Includes | IncludedBy | NoOverlap deriving Eq
-
-comp :: Prefix -> Prefix -> Overlaps
-comp (Prefix (!l1,!v1)) (Prefix (!l2,!v2)) | l1 == l2 = NoOverlap
-                                           | l1 < l2 && mask l1 v1 == mask l1 v2 = Includes
-                                           | l1 > l2 && mask l2 v1 == mask l2 v2 = IncludedBy 
-                                           | otherwise = NoOverlap
-                                           where mask l v = v .&. shiftR (fromIntegral l) 0xffffffff
-
--- insert logic
-
--- insert x (Tree a 
+instance Overlap Prefix where
+    comp (Prefix (!l1,!v1)) (Prefix (!l2,!v2)) 
+                                               | l1 == l2 && v1 == v2 = Equal
+                                               | l1 == l2 = NoOverlap
+                                               | l1 < l2 && mask l1 v1 == mask l1 v2 = Includes
+                                               | l1 > l2 && mask l2 v1 == mask l2 v2 = IncludedBy 
+                                               | otherwise = NoOverlap
+                                               where mask l v = v .&. shiftR (fromIntegral l) 0xffffffff
 
 data Node a = Empty | Item a (Node a) (Node a) deriving Show
-root = Item defaultRoute Empty Empty
 
 singleton x = Item x Empty Empty
 insert Empty x = singleton x
-insert (Item a b c) x | overlap == NoOverlap  = Item a (insert b x) c
+insert (Item a b c) x
+                      | overlap == Equal = Item a b c
+                      | overlap == NoOverlap  = Item a (insert b x) c
                       | overlap == Includes   = Item a b (insert c x)
                       | overlap == IncludedBy = Item x Empty (Item a b c )
-                        where overlap = comp x a
+                        where overlap = comp a x
+
+fromList :: Overlap a => [a] -> Node a
+fromList = foldl' insert Empty
