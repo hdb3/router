29fa167b12b37b48c805786047f64f94efc837e9
Nicholas Hart <nic@hdb3.net>
2018-08-23 12:31:10 +0100

    change Node to Tree

--- Overlap.hs
+++ Overlap.hs
@@ -5,7 +5,7 @@ import Data.List(foldl')
 import Data.Bits
 import Prefixes
 
-type PrefixTree = Node Prefix
+type PrefixTree = Tree Prefix
 
 data Overlaps = Includes | IncludedBy | NoOverlap | Equal deriving Eq
 class Overlap a where
@@ -20,17 +20,17 @@ instance Overlap Prefix where
                                                | otherwise = NoOverlap
                                                where mask l v = v .&. shiftR (fromIntegral l) 0xffffffff
 
-data Node a = Empty | Item a (Node a) (Node a) deriving Show
+data Tree a = Empty | Item a (Tree a) (Tree a) deriving Show
 
-instance Functor Node where
+instance Functor Tree where
     fmap _ Empty = Empty
     fmap f (Item a b c ) = Item (f a) (fmap f b) (fmap f c)  
 
-instance Foldable Node where
+instance Foldable Tree where
     foldr f z Empty = z
     foldr f z (Item a b c ) = foldr f (foldr f (f a z) b) c
 
-size :: Node a -> Int
+size :: Tree a -> Int
 size = foldr (\_ b -> b+1) 0
 
 singleton x = Item x Empty Empty
@@ -42,16 +42,16 @@ insert (Item a b c) x
                       | overlap == IncludedBy = Item x Empty (Item a b c )
                         where overlap = comp a x
 
-fromList :: Overlap a => [a] -> Node a
+fromList :: Overlap a => [a] -> Tree a
 fromList = foldl' insert Empty
 
-width :: Node a -> Int
+width :: Tree a -> Int
 width t = max j k where
     (j,k) = w (0,0) t
     w (a,b) Empty = (a,b)
     w (a,b) (Item _ x y) = w (a+1, max b (width y)) x 
 
-height :: Node a -> Int
+height :: Tree a -> Int
 height t = max j k where
     (j,k) = h (0,0) t
     h (a,b) Empty = (a,b)
