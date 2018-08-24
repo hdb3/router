0b5294ade1d008421ab036ab18429d4b6247d1c5
Nicholas Hart <nic@hdb3.net>
2018-08-24 14:35:53 +0100

    add Overlap2.hs new functions such as partition, reduce and toList

--- Overlap2.hs
+++ Overlap2.hs
@@ -1,5 +1,5 @@
 {-#LANGUAGE OverloadedStrings #-}
-module Overlap2(Tree(Empty),PrefixTree,height,size,fromList,insertPrefix) where
+module Overlap2(Tree(Empty),PrefixTree,height,size,toList,fromList,insertPrefix,reduce,count,longest,partition) where
 import Data.List(foldl')
 import Data.Bits(testBit)
 import Data.Word
@@ -7,9 +7,10 @@ import Prefixes
 
 type PrefixTree = Tree Prefix
 
-data Tree a = Empty | Item (Maybe a) (Tree a) (Tree a)
+-- TODO - make the Tree gereic in Insert etc.....
+data Tree a = Empty | Item (Maybe a) (Tree a) (Tree a) deriving Eq
 instance Show a => Show (Tree a) where
-    show = showN where
+    show = showRL where
 
         showRL Empty = ""
         showRL (Item (Just a) b c) = "[" ++ show a ++ "]" ++ show b ++ show c
@@ -18,8 +19,6 @@ instance Show a => Show (Tree a) where
         showRL (Item Nothing b c) = "RL" ++ show b ++ show c
 
         showN (Item (Just a) b c) = "(" ++ show a ++ "," ++ show b ++ "," ++ show c ++ ")"
-        -- show (Item Nothing Empty c) = show c
-        -- show (Item Nothing b Empty) = show b
         showN (Item Nothing b c) = "(*," ++ show b ++ "," ++ show c ++ ")"
         showN Empty = "-"
 
@@ -48,7 +47,14 @@ instance Foldable Tree where
     foldr f z (Item Nothing b c ) = foldr f (foldr f z b) c
     foldr f z (Item (Just a) b c ) = foldr f (foldr f (f a z) b) c
 
+count :: Tree a -> Int
+count Empty = 0
+count (Item _ b c ) = 1 + count b + count c
+
 singleton x = Item (Just x) Empty Empty
+
+toList :: PrefixTree -> [Prefix]
+toList t = foldr (:) [] t
 
 fromList :: [Prefix] -> PrefixTree
 fromList = foldl' (flip insertPrefix) Empty
@@ -56,7 +62,30 @@ fromList = foldl' (flip insertPrefix) Empty
 size :: Tree a -> Int
 size = foldr (\_ b -> b+1) 0
 
+-- this version of longest took 11 minutes to run over a full internet route table
+-- the longets sequence discovered was 
+-- [222.32.0.0/11,222.35.0.0/16,222.35.128.0/17,222.35.128.0/18,222.35.128.0/19,222.35.136.0/21,222.35.136.0/22,222.35.137.0/24]
+-- and also validated the height function
+
+longest :: Tree a -> [a]
+longest Empty = []
+longest (Item Nothing b c) = if (length $ longest c) > (length $ longest b) then longest c else longest b
+longest (Item (Just a) b c) = a : (longest (Item Nothing b c))
+
 height :: Tree a -> Int
-height Empty = 0
-height (Item Nothing b c) = max (height b) (height c)
-height (Item _ b c) = 1 + max (height b) (height c)
+-- height = height' . reduce where
+height = height' where
+    height' Empty = 0
+    height' (Item Nothing b c) = max (height' b) (height' c)
+    height' (Item _ b c) = 1 + max (height' b) (height' c)
+
+reduce :: Tree a -> Tree a
+reduce Empty = Empty
+reduce (Item Nothing Empty c) = reduce c
+reduce (Item Nothing b Empty)  = reduce b
+reduce (Item a b c) = Item a (reduce b) (reduce c)
+
+partition :: Tree a -> [Tree a]
+partition Empty = []
+partition (Item Nothing b c) = partition b ++ partition c
+partition t = [t]
