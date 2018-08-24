45c23adba859cf2945a65f92cab59c237e8d9b78
Nicholas Hart <nic@hdb3.net>
2018-08-24 14:36:26 +0100

    OverlapReader.hs now does some partition analysis

--- OverlapReader.hs
+++ OverlapReader.hs
@@ -1,26 +1,48 @@
 module Main where
 import System.IO
-import Data.List(sortOn)
+import qualified Data.List
 
 import Prefixes
 import BGPReader(readRib)
-import Overlap
+import qualified Overlap2 as Overlap
 
 main = do
+    let sortPrefixes = Data.List.sortOn tuple where tuple (Prefix (a,b)) = (a,b)
+        len (Prefix (a,b)) = a
+
     rib <- readRib
     putStrLn $ "got " ++ show (length rib) ++ " routes"
     -- print (last rib)
     let allPrefixes = concatMap snd rib
         -- drop default if present and also every thing longer than /24
-        (real,fake) = span ((25 >) . l) $ dropWhile ((0 ==) . l) $ sortOn l allPrefixes
-        l (Prefix (a,b)) = a
+        (real,fake) = span ((25 >) . len) $ dropWhile ((0 ==) . len) $ sortPrefixes allPrefixes
         
     putStrLn $ "got " ++ show (length real) ++ " prefixes"
-    print $ ("real", take 5 real)
-    print $ ("fake",fake)
-{-
-    let t = Overlap.fromList sortedAllPrefixes
-    putStrLn $ "tree contains " ++ show (size t)
-    putStrLn $ "tree height   " ++ show (height t)
-    putStrLn $ "tree width    " ++ show (width t)
--}
+
+    -- these are useful sanity checks...
+    -- print $ ("real", take 5 real)
+    -- print $ ("fake",fake)
+
+    let t = Overlap.fromList real
+        r = Overlap.reduce t
+        p = Overlap.partition r
+        t' = sortPrefixes $ Overlap.toList r
+        p' = sortPrefixes $ concatMap Overlap.toList p
+        (single,multiple) = Data.List.partition ( (2 >) . Overlap.size) p
+
+    if real /= t' then putStrLn "consistency check on Overlap.fromList / Overlap.toList FAILS!!!!" else return ()
+    if real /= p' then putStrLn "consistency check on Overlap.partition FAILS!!!!" else return ()
+    -- putStrLn $ "consistency check on Overlap.fromList / Overlap.toList " ++ if real == t' then "Success!" else "FAILS!!!!"
+
+    putStrLn $ "tree contains " ++ show (Overlap.size t)
+    putStrLn $ "tree height   " ++ show (Overlap.height t)
+    -- putStrLn $ "longest   " ++ show (Overlap.longest t)
+    putStrLn $ "tree contains " ++ show (length p) ++ " partitions"
+    putStrLn $ show (length single) ++ " non-overlapping and " ++ show (length multiple) ++ " overlapping partitions"
+    putStrLn $ "full distribution by size: " ++ show (distribution $ map Overlap.size p)
+    putStrLn $ "full distribution by height: " ++ show (distribution $ map Overlap.height p)
+
+
+distribution :: [Int] -> [(Int,Int)]
+distribution = map (\a -> (head a,length a)) . Data.List.group . Data.List.sort
+
