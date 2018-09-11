87a864e412c20eeb0c768f41463ad728657185cf
Nicholas Hart <nic@hdb3.net>
2018-09-11 11:57:15 +0100

    WiP

--- ActPassive.hs
+++ ActPassive.hs
@@ -15,66 +15,86 @@ import BgpFSM
 import Collision
 import Args2
 import Rib
+import BGPReader(pathReadRib)
+import Update(makeUpdate)
 
 main :: IO ()
-main = do config <- getConfig
-          either putStrLn
-                 main'
-                 config
+main = do
+    peers <- getConfig
+    either
+        putStrLn
+        start
+        peers
 
-main' peers = do
+start peers = do
+    putStrLn "ActPassive starting"
     print peers
 
     let global = globalData (head peers)
-        address = SockAddrInet bgpPort 0 -- listen on all intefaces by default...
+        listenAddress = SockAddrInet bgpPort 0 -- listen on all intefaces by default...
         local = localPeer global
-
-    let peerMap = Data.Map.fromList $ map (\pd -> (peerIPv4 pd,pd)) peers
-    print peerMap
-
-    putStrLn "ActPassive starting"
-    sock <- socket AF_INET Stream defaultProtocol 
-    setSocketOption sock ReuseAddr 1
-    bind sock address
-    listen sock 100
+        peerMap = Data.Map.fromList $ map (\pd -> (peerIPv4 pd,pd)) peers
     collisionDetector <- mkCollisionDetector
     exitMVar <- newEmptyMVar
-    sessions <- newMVar ( Data.Map.empty :: Data.Map.Map ThreadId PeerData )
+    sessions <- newMVar ( Data.Map.empty :: Data.Map.Map ThreadId (IPv4,PeerData) )
     rib <- Rib.newRib local
     insertStatic rib local
+    let commons = (rib,peerMap,exitMVar,collisionDetector,sessions,global,local,listenAddress)
     putStrLn "ActPassive ready"
-    forkIO $ reaper exitMVar rib sessions
-    E.finally (loop (sock,rib,peerMap,exitMVar,collisionDetector,sessions) )
-              (close sock)
 
-reaper mbox rib sessions = forever $ do
-    (tid,peerName,es) <- takeMVar mbox
-    either
-        (\s -> putStrLn $ "thread " ++ show tid ++ "/" ++ show peerName ++ " exited with exception <" ++ s ++ ">")
-        (\s -> putStrLn $ "thread " ++ show tid ++ "/" ++ show peerName ++ " exited normally <" ++ s ++ ">")
-        es
+    forkIO $ reaper commons
+    forkIO $ listener commons 
+    mapM_ (connectTo commons) peers
+    return ()
+
+reaper commons = 
+    let (rib,peerMap,exitMVar,collisionDetector,sessions,global,local,listenAddress) = commons
+    in forever $ do
+        putStrLn "reaper"
+        (tid,peerName,es) <- takeMVar exitMVar
+        either
+            (\s -> putStrLn $ "thread " ++ show tid ++ "/" ++ show peerName ++ " exited with exception <" ++ s ++ ">")
+            (\s -> putStrLn $ "thread " ++ show tid ++ "/" ++ show peerName ++ " exited normally <" ++ s ++ ">")
+            es
+        threadMap <- takeMVar sessions
+        delPeer rib ( snd (threadMap Data.Map.! tid ))
+        putMVar sessions ( Data.Map.delete tid threadMap )
+
+listener commons = do
+    let (_,peerMap,_,_,_,_,_,listenAddress) = commons
+    sock <- socket AF_INET Stream defaultProtocol 
+    setSocketOption sock ReuseAddr 1
+    bind sock listenAddress
+    listen sock 100
+    forever $ do
+        (conn, peerAddress) <- accept sock
+        putStrLn "listener"
+        maybe
+            ( putStrLn $ "Reject connection from " ++ show peerAddress )
+            ( \peerData -> startFSM (conn,peerAddress,peerData,commons))
+            (getPeerConfig commons peerAddress)
+
+defaultPeerData = Nothing -- change to permit unconfigured inbound connections
+getPeerConfig commons peerAddress = maybe defaultPeerData Just (getPeerConfig commons peerAddress)
+getPeerConfig' (_,peerMap,_,_,_,_,_,_) peerAddress = Data.Map.lookup (getIPv4 peerAddress) peerMap
+
+
+connectTo commons peerData = do
+    conn <- socket AF_INET Stream defaultProtocol
+    Network.Socket.connect conn ( SockAddrInet bgpPort (toHostAddress $ peerIPv4 peerData))
+    peerName <- getPeerName conn
+    putStrLn $ "connected outbound to: " ++ (show peerName)
+    startFSM (conn,peerName,peerData,commons)
+
+startFSM (conn,peerAddress,peerData,commons) = do
+    let (rib,peerMap,exitMVar,collisionDetector,sessions,global,local,listenAddress) = commons
+    putStrLn $ "starting session for " ++ show (getIPv4 peerAddress)
+    logfile <- getLogFile
+    let delayOpenTimer = 10
+    let config = BgpFSMconfig conn collisionDetector peerAddress delayOpenTimer exitMVar logfile peerData rib
+    threadId <- forkIO (bgpFSM config)
     threadMap <- takeMVar sessions
-    -- let peerData = threadMap ! tid
-    delPeer rib ( threadMap Data.Map.! tid )
-    putMVar sessions ( Data.Map.delete tid threadMap )
-
-
-loop (sock,rib,peerMap,exitMVar,collisionDetector,sessions) = forever $ do
-    (conn, peer) <- accept sock
-    let peerIPv4 = getIPv4 peer
-        delayOpenTimer = 10
-    maybe
-        ( putStrLn $ "Reject connection from " ++ show peer )
-        ( \peerData -> do
-            putStrLn $ "Connection from " ++ show peer
-            logfile <- getLogFile
-            let config = BgpFSMconfig conn collisionDetector peer delayOpenTimer exitMVar logfile peerData rib
-            threadId <- forkIO (bgpFSM config)
-            threadMap <- takeMVar sessions
-            putMVar sessions ( Data.Map.insert threadId peerData threadMap )
-        )
-        ( Data.Map.lookup peerIPv4 peerMap )
-
+    putMVar sessions ( Data.Map.insert threadId ((getIPv4 peerAddress),peerData) threadMap )
 
 getIPv4 :: SockAddr -> Data.IP.IPv4
 getIPv4 (SockAddrInet portNumber hostAddress) = fromHostAddress hostAddress
@@ -87,10 +107,10 @@ getLogFile = do
 
 insertStatic rib local = do
     -- pathReadRib :: FilePath -> IO [((Int, [PathAttributes.PathAttribute]), [Prefixes.Prefix])]
-    -- updates <- pathReadRib "bgpdata/full.bgp"
+    updates <- pathReadRib "bgpdata/full.bgp"
     -- ribUpdater :: Rib -> PeerData -> ParsedUpdate -> IO()
     -- makeUpdate :: [Prefix] -> [Prefix] -> [PathAttribute] -> ParsedUpdate
-    -- let updates' = concatMap (\((_,pas),pfxs) -> makeUpdate pfxs [] pas) (take 1000 updates)
+    let updates' = concatMap (\((_,pas),pfxs) -> makeUpdate pfxs [] pas) (take 1000 updates)
     -- mapM print updates'
     -- mapM (ribUpdater rib local) updates'
     return ()
