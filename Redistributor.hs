1a883f72883ec68ac87812763c04f52bcabc09c9
Nicholas Hart <nic@hdb3.net>
2018-10-07 18:40:16 +0100

    add route delete using zserv

--- Redistributor.hs
+++ Redistributor.hs
@@ -11,6 +11,7 @@ import Control.Monad(when,unless)
 import Data.Maybe(fromJust,isJust,fromMaybe)
 import Data.Either(either)
 import qualified Data.Map.Strict as Data.Map
+import qualified System.IO.Streams as Streams
 
 import BGPRib
 import BGPlib
@@ -27,17 +28,20 @@ redistribute global@Global{..} =
        putStrLn $ "Thread " ++ show threadId ++ " starting redistributor"
        ( zStreamIn, zStreamOut ) <- getZStreamUnix "/var/run/quagga/zserv.api"
        zservRegister zStreamOut _ZEBRA_ROUTE_BGP
-       let dummyRouteInstaller (route, Nothing) = putStrLn $ "route not in Rib!: " ++ show route
-           dummyRouteInstaller (route, Just nextHop) = do putStrLn $ "install " ++ show route ++ " via " ++ show nextHop
-                                                          addRoute zStreamOut (toAddrRange $ toPrefix route) nextHop
+       forkIO (zservReader ( zStreamIn, zStreamOut ))
+       let routeInstall (route, Nothing) = putStrLn $ "route not in Rib!: " ++ show route
+           routeInstall (route, Just nextHop) = do putStrLn $ "install " ++ show route ++ " via " ++ show nextHop
+                                                   addRoute zStreamOut (toAddrRange $ toPrefix route) nextHop
+           routeDelete route = do putStrLn $ "delete " ++ show route
+                                  delRoute zStreamOut (toAddrRange $ toPrefix route)
 
         -- addPeer rib peerData
         -- delPeer rib peerData
         -- BGPRib.ribUpdater rib peerData parsedUpdate
-       ribUpdateListener dummyRouteInstaller rib ( localPeer gd ) 1
+       ribUpdateListener (routeInstall,routeDelete) rib ( localPeer gd ) 1
 
 
-ribUpdateListener routeInstaller rib peer timeout = do
+ribUpdateListener (routeInstall,routeDelete) rib peer timeout = do
     updates <- pullAllUpdates (1000000 * timeout) peer rib
     if null updates then
         yield -- null op - could check if exit from thread is needed...
@@ -50,10 +54,28 @@ ribUpdateListener routeInstaller rib peer timeout = do
                 putStrLn $ "and " ++ show (length updates - 10) ++ " more"
             let getNextHop rib pfx = do nh <- lookupNextHop rib pfx
                                         return (pfx,nh)
-            routes <- mapM ( getNextHop rib ) (concatMap fst updates)
-            mapM_ routeInstaller routes
+            let (update,withdraw) = foldl disc ([],[]) updates
+                disc (u,w) (pfxs,0) = (u,w++pfxs) -- withdraw has 0 for the route index
+                disc (u,w) (pfxs,_) = (u++pfxs,w) -- alternate case is an update, not withdraw - and we don't care what was the route index at the time...
+            routes <- mapM ( getNextHop rib ) update
+            mapM_ routeDelete withdraw
+            mapM_ routeInstall routes
 
     -- rinse and repeat...
 
-    ribUpdateListener routeInstaller rib peer timeout
+    ribUpdateListener (routeInstall,routeDelete) rib peer timeout
+
+
+zservReader ( zStreamIn, zStreamOut ) = do
+    zservRequestRouterId zStreamOut
+    zservRequestInterface zStreamOut
+    loop zStreamIn
+    where
+    loop stream = do
+        msg <- Streams.read stream
+        maybe (putStrLn "end of messages")
+              ( \zMsg -> do 
+                              print zMsg
+                              loop stream )
+              msg
 
