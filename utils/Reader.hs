46f63a9849d2f8bcdcdb9070ac9829dfd75b0450
Nicholas Hart <nic@hdb3.net>
2018-12-13 18:43:59 +0000

    Reader.hs is now a non-RIB analysis tool, old Reader.hs is now RibReader.hs

--- utils/Reader.hs
+++ utils/Reader.hs
@@ -1,8 +1,29 @@
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import BGPReader(readRib)
+import BGPReader(readMsgs)
+-- import BGPlib
+import BGPlib(Prefix,BGPMessage(..))
+import Data.Binary
 
 main = do
-    rib <- readRib
-    putStrLn $ "got " ++ show (length rib) ++ " routes"
-    print (last rib)
+    msgs <- readMsgs
+    putStrLn $ "got " ++ show (length msgs) ++ " messages"
+    analyseMessageTypes msgs
+    analysePrefixes msgs
+
+analyseMessageTypes msgs = do
+    let groups = foldl f ([],[],[],[]) msgs
+        f (o,u,k,n) open@BGPOpen{..}           = (open:o,u,k,n)
+        f (o,u,k,n) update@BGPUpdate{..}       = (o,update:u,k,n)
+        f (o,u,k,n) keepalive@BGPKeepalive     = (o,u,keepalive:k,n)
+        f (o,u,k,n) notification@BGPNotify{..} = (o,u,k,notification:n)
+        count (o,u,k,n) = (length o,length u,length k,length n)
+    putStrLn $ "count (o,u,k,n) = " ++ show (count groups)
+
+analysePrefixes msgs = do
+    let prefixes = foldl f (0,0) msgs
+        prefixCount bs = length (decode bs :: [Prefix]) 
+        f (u,w) BGPUpdate{..} = ( u + prefixCount nlri , w + prefixCount withdrawn )
+        f (u,w) _ = (u,w)
+    putStrLn $ "count (update,withdrawn) = " ++ show prefixes
